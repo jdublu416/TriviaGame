049083f8bfaacec338bcce8c347d69d74b094283
Janet Warren <33846232+jdublu416@users.noreply.github.com>
2018-02-28 00:12:51 -0500

    trivia game framework and css grid, images updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+//create an array of questions that correspond to an array of answers
+
+var questions=[];
+var answer0=[];
+var answer1=[];
+var answer2=[];
+var answer3=[];
+var answer4=[];
+
+
+
+
+
+
+
+
+
+
+
+});
